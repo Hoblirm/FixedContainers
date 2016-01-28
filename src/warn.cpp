43eefc1b766dc1b28bf3ffa1cdc1596d08ff3397
Richard Hoblitzell <hoblirm@gmail.com>
2016-01-27 22:41:04 -0500

    added warn file.

---
+++ src/warn.cpp
@@ -0,0 +1,11 @@
+#include <fixed_array.h>
+#include <fixed_vector.h>
+int main(int argc, char** argv)
+{
+
+fixed_array<int, 8> a;
+fixed_array<int> b(8);
+fixed_vector<int, 8> c;
+fixed_vector<int> d(8);
+  return 0;
+}
