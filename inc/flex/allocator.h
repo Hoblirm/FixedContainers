f843bc1376918c529cc8a5f33bb0774f38561b90
Richard Hoblitzell <hoblirm@gmail.com>
2016-03-02 22:08:13 -0500

    inlined allocator methods...

--- inc/flex/allocator.h
+++ inc/flex/allocator.h
@@ -39,31 +39,31 @@ namespace flex
     /* constructors and destructor
      * - nothing to do because the allocator has no state
      */
-    allocator()
+    inline allocator()
     {
     }
 
-    allocator(const allocator&)
+    inline allocator(const allocator&)
     {
     }
 
     template<class U>
-    allocator(const allocator<U>&)
+    inline allocator(const allocator<U>&)
     {
     }
 
-    ~allocator()
+    inline ~allocator()
     {
     }
 
     // return maximum number of elements that can be allocated
-    size_type max_size() const
+    inline size_type max_size() const
     {
       return std::numeric_limits<std::size_t>::max() / sizeof(T);
     }
 
     // allocate but don't initialize num elements of type T
-    pointer allocate(size_type num, typename std::allocator<void>::const_pointer = 0)
+    inline pointer allocate(size_type num, typename std::allocator<void>::const_pointer = 0)
     {
       if (allocation_guard::is_enabled())
       {
@@ -73,21 +73,21 @@ namespace flex
     }
 
     // initialize elements of allocated storage p with value value
-    void construct(pointer p, const T& value)
+    inline void construct(pointer p, const T& value)
     {
       // initialize memory with placement new
       new ((void*) p) T(value);
     }
 
     // destroy elements of initialized storage p
-    void destroy(pointer p)
+    inline void destroy(pointer p)
     {
       // destroy objects by calling their destructor
       p->~T();
     }
 
     // deallocate storage p of deleted elements
-    void deallocate(pointer p, size_type num)
+    inline void deallocate(pointer p, size_type num)
     {
       ::operator delete((void*) p);
     }
