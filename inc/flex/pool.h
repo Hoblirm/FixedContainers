bb4f42469f0520c91c7a403aeba3d0ba8ea2d377
Richard Hoblitzell <hoblirm@gmail.com>
2016-03-29 20:34:39 -0400

    Got initial pool tests passing.

--- inc/flex/pool.h
+++ inc/flex/pool.h
@@ -6,164 +6,164 @@
 namespace flex
 {
 
-struct Link
-{
-	Link* mNext;
-};
+  struct Link
+  {
+    Link* mNext;
+  };
 
-template<size_t N>
-struct PoolNode
-{
-	char data[N];
-};
+  template<size_t N>
+  struct PoolNode
+  {
+    char data[N];
+  };
 
-template<class T/*, class Alloc = flex::allocator<PoolNode<(sizeof(T) < sizeof(Link)) ? sizeof(Link) : sizeof(T)>  >*/> class pool: guarded_object
-{
-public:
-	typedef T value_type;
-	typedef T* pointer;
-	typedef const T* const_pointer;
-	typedef T& reference;
-	typedef const T& const_reference;
-	typedef size_t size_type;
+  template<class T/*, class Alloc = flex::allocator<PoolNode<(sizeof(T) < sizeof(Link)) ? sizeof(Link) : sizeof(T)>  >*/> class pool: guarded_object
+  {
+  public:
+    typedef T value_type;
+    typedef T* pointer;
+    typedef const T* const_pointer;
+    typedef T& reference;
+    typedef const T& const_reference;
+    typedef size_t size_type;
 
-	pool();
-	explicit pool(size_type n);
-	~pool();
+    pool();
+    explicit pool(size_type n);
+    ~pool();
 
-	void* allocate();
-	pointer construct();
-	pointer construct(const value_type& val);
-	void destruct(pointer ptr);
-	void deallocate(void* ptr);
-	bool empty() const;
-	pool& operator=(const pool&);
-	void reserve(size_type n);
-	size_type size() const;
+    void* allocate();
+    pointer construct();
+    pointer construct(const value_type& val);
+    void destruct(pointer ptr);
+    void deallocate(void* ptr);
+    bool empty() const;
+    pool& operator=(const pool&);
+    void reserve(size_type n);
+    size_type size() const;
 
-protected:
-	Link* mHead;
-	size_t mObjectSize;
-	bool mFixed;
-};
+  protected:
+    Link* mHead;
+    size_t mObjectSize;
+    bool mFixed;
+  };
 
-template<class T>
-inline pool<T>::pool() :
-		mHead(NULL), mObjectSize((sizeof(T) < sizeof(Link)) ? sizeof(Link) : sizeof(T))
-{
-}
+  template<class T>
+  inline pool<T>::pool() :
+      mHead(NULL), mObjectSize((sizeof(T) < sizeof(Link)) ? sizeof(Link) : sizeof(T)), mFixed(false)
+  {
+  }
 
-template<class T>
-inline pool<T>::pool(size_t n) :
-		mHead(NULL), mObjectSize((sizeof(T) < sizeof(Link)) ? sizeof(Link) : sizeof(T))
-{
-	reserve(n);
-}
+  template<class T>
+  inline pool<T>::pool(size_t n) :
+      mHead(NULL), mObjectSize((sizeof(T) < sizeof(Link)) ? sizeof(Link) : sizeof(T)), mFixed(false)
+  {
+    reserve(n);
+  }
 
-template<class T>
-inline pool<T>::~pool()
-{
-	if (!mFixed)
-	{
-		//Only want to retrieve pointers when mHead is set.  Otherwise the pool will internally
-		//allocate new objects.
-		while (mHead)
-		{
-			//Allocate retrieves an available pointer in the pool which we will then delete.
-			void* ptr = allocate();
-			::operator delete(ptr);
-		}
-	}
-}
+  template<class T>
+  inline pool<T>::~pool()
+  {
+    if (!mFixed)
+    {
+      //Only want to retrieve pointers when mHead is set.  Otherwise the pool will internally
+      //allocate new objects.
+      while (mHead)
+      {
+        //Allocate retrieves an available pointer in the pool which we will then delete.
+        void* ptr = allocate();
+        ::operator delete(ptr);
+      }
+    }
+  }
 
-template<class T>
-inline void* pool<T>::allocate()
-{
-	if (mHead)
-	{
-		Link* ptr = mHead;
-		mHead = ptr->mNext;
-		return ptr;
-	}
-	else
-	{
-		if (FLEX_UNLIKELY(mFixed || sAllocationGuardEnabled))
-		{
-			throw std::runtime_error("pool: performed runtime allocation");
-		}
-		return ::operator new(mObjectSize);
-	}
-}
+  template<class T>
+  inline void* pool<T>::allocate()
+  {
+    if (mHead)
+    {
+      Link* ptr = mHead;
+      mHead = ptr->mNext;
+      return ptr;
+    }
+    else
+    {
+      if (FLEX_UNLIKELY(mFixed || sAllocationGuardEnabled))
+      {
+        throw std::runtime_error("pool: performed runtime allocation");
+      }
+      return ::operator new(mObjectSize);
+    }
+  }
 
-template<class T>
-inline typename pool<T>::pointer pool<T>::construct()
-{
-	void* ptr = allocate();
-	new (ptr) value_type();
-	return (pointer) ptr;
-}
+  template<class T>
+  inline typename pool<T>::pointer pool<T>::construct()
+  {
+    void* ptr = allocate();
+    new (ptr) value_type();
+    return (pointer) ptr;
+  }
 
-template<class T>
-inline typename pool<T>::pointer pool<T>::construct(const value_type& val)
-{
-	void* ptr = allocate();
-	new (ptr) value_type(val);
-	return (pointer) ptr;
-}
+  template<class T>
+  inline typename pool<T>::pointer pool<T>::construct(const value_type& val)
+  {
+    void* ptr = allocate();
+    new (ptr) value_type(val);
+    return (pointer) ptr;
+  }
 
-template<class T>
-inline void pool<T>::deallocate(void* ptr)
-{
-	((Link*) ptr)->mNext = mHead;
-	mHead = ((Link*) ptr);
-}
+  template<class T>
+  inline void pool<T>::deallocate(void* ptr)
+  {
+    ((Link*) ptr)->mNext = mHead;
+    mHead = ((Link*) ptr);
+  }
 
-template<class T>
-inline void pool<T>::destruct(pointer ptr)
-{
-	ptr->~value_type();
-	deallocate(ptr);
-}
+  template<class T>
+  inline void pool<T>::destruct(pointer ptr)
+  {
+    ptr->~value_type();
+    deallocate(ptr);
+  }
 
-template<class T>
-inline bool pool<T>::empty() const
-{
-	return (mHead == NULL);
-}
+  template<class T>
+  inline bool pool<T>::empty() const
+  {
+    return (mHead == NULL);
+  }
 
-template<class T>
-inline typename pool<T>::size_type pool<T>::size() const
-{
-	size_type n = 0;
-	for (Link* it = mHead; it; it = it->mNext)
-	{
-		++n;
-	}
-	return n;
-}
+  template<class T>
+  inline typename pool<T>::size_type pool<T>::size() const
+  {
+    size_type n = 0;
+    for (Link* it = mHead; it; it = it->mNext)
+    {
+      ++n;
+    }
+    return n;
+  }
 
-template<class T>
-inline pool<T>& pool<T>::operator=(const pool&)
-{
-	return *this;
-}
+  template<class T>
+  inline pool<T>& pool<T>::operator=(const pool&)
+  {
+    return *this;
+  }
 
-template<class T>
-inline void pool<T>::reserve(size_type n)
-{
-	for (; n; --n)
-	{
-		if (FLEX_UNLIKELY(mFixed || sAllocationGuardEnabled))
-		{
-			throw std::runtime_error("pool: performed runtime allocation");
-		}
+  template<class T>
+  inline void pool<T>::reserve(size_type n)
+  {
+    for (; n; --n)
+    {
+      if (FLEX_UNLIKELY(mFixed || sAllocationGuardEnabled))
+      {
+        throw std::runtime_error("pool: performed runtime allocation");
+      }
 
-		//The pool's deallocate method simply adds a new pointer to the pool. Despite the
-		//name, it doesn't perform an actual deallocation.  In this method, it is treated like
-		//a push_front() method.  We simply allocate new objects, and push the pointers to the pool.
-		deallocate(::operator new(mObjectSize));
-	}
-}
+      //The pool's deallocate method simply adds a new pointer to the pool. Despite the
+      //name, it doesn't perform an actual deallocation.  In this method, it is treated like
+      //a push_front() method.  We simply allocate a new object, and push the pointer to the pool.
+      deallocate(::operator new(mObjectSize));
+    }
+  }
 
 } //namespace flex
 #endif /* FLEX_POOL_H */
--- test/inc/pool_test.h
+++ test/inc/pool_test.h
@@ -9,249 +9,249 @@ class pool_test: public CxxTest::TestSuite
 {
 public:
 
-	struct obj
-	{
-		static const int DEFAULT_VAL = 1;
-		static const int INIT_KEY = 858599509;
+  struct obj
+  {
+    static const int DEFAULT_VAL = 1;
+    static const int INIT_KEY = 858599509;
 
-		obj() :
-				val(DEFAULT_VAL), init(INIT_KEY)
-		{
-		}
+    obj() :
+        val(DEFAULT_VAL), init(INIT_KEY)
+    {
+    }
 
-		obj(int i) :
-				val(i), init(INIT_KEY)
-		{
-		}
+    obj(int i) :
+        val(i), init(INIT_KEY)
+    {
+    }
 
-		~obj()
-		{
-			init = 0;
-		}
+    ~obj()
+    {
+      init = 0;
+    }
 
-		obj& operator=(const obj& o)
-		{
-			val = o.val;
-			return *this;
-		}
+    obj& operator=(const obj& o)
+    {
+      val = o.val;
+      return *this;
+    }
 
-		operator int() const
-		{
-			return val;
-		}
+    operator int() const
+    {
+      return val;
+    }
 
-		int val;
-		int init;
-	};
+    int val;
+    int init;
+  };
 
-	bool is_container_valid(pool& c)
-	{
-		bool is_valid = true;
-		flex::vector<void*> v;
-		while (!c.empty())
-		{
-			void* ptr = c.allocate();
-			if (((obj*) ptr)->init == INIT_KEY)
-			{
-				is_valid = false;
-			}
-			v.push_back(c.allocate());
-		}
-		while (!v.empty())
-		{
-			c.deallocate(b.back());
-			b.pop_back();
-		}
-		return is_valid;
-	}
+  bool is_container_valid(pool<obj>& c)
+  {
+    bool is_valid = true;
+    flex::fixed_vector<void*, 128> v;
+    while (!c.empty())
+    {
+      void* ptr = c.allocate();
+      if (((obj*) ptr)->init == obj::INIT_KEY)
+      {
+        is_valid = false;
+      }
+      v.push_back(ptr);
+    }
+    while (!v.empty())
+    {
+      c.deallocate(v.back());
+      v.pop_back();
+    }
+    return is_valid;
+  }
 
-	void test_default_constructor()
-	{
-		/*
-		 * Case1: Ensure it doesn't allocate memory.
-		 */
-		flex::allocation_guard::enable();
-		pool<obj> a;
-		TS_ASSERT(is_container_valid(a));
-		TS_ASSERT_EQUALS(a.size(), 0);
-		flex::allocation_guard::disable();
-	}
+  void test_default_constructor()
+  {
+    /*
+     * Case1: Ensure it doesn't allocate memory.
+     */
+    flex::allocation_guard::enable();
+    pool<obj> a;
+    TS_ASSERT(is_container_valid(a));
+    TS_ASSERT_EQUALS(a.size(), 0);
+    flex::allocation_guard::disable();
+  }
 
-	void test_fill_constructor()
-	{
-		/*
-		 * Case1: Ensure it reserves memory.
-		 */
-		pool<obj> a(16);
-		TS_ASSERT(is_container_valid(a));
-		TS_ASSERT_EQUALS(a.size(), 16);
+  void test_fill_constructor()
+  {
+    /*
+     * Case1: Ensure it reserves memory.
+     */
+    pool<obj> a(16);
+    TS_ASSERT(is_container_valid(a));
+    TS_ASSERT_EQUALS(a.size(), 16);
 
-		/*
-		 * Case2: Ensure it throws if allocation guard is enabled.
-		 */
-		flex::allocation_guard::enable();
-		TS_ASSERT_THROWS(pool<obj> b(16), std::runtime_error)
-		flex::allocation_guard::disable();
-	}
+    /*
+     * Case2: Ensure it throws if allocation guard is enabled.
+     */
+    flex::allocation_guard::enable();
+    TS_ASSERT_THROWS(pool<obj> b(16), std::runtime_error)
+    flex::allocation_guard::disable();
+  }
 
-	void test_destructor()
-	{
-		/*
-		 * Case1: Ensure it works on empty container.
-		 */
-		pool<obj> a;
-		a.~pool();
-		TS_ASSERT(is_container_valid(a));
+  void test_destructor()
+  {
+    /*
+     * Case1: Ensure it works on empty container.
+     */
+    pool<obj> a;
+    a.~pool();
+    TS_ASSERT(is_container_valid(a));
 
-		/*
-		 * Case2: Ensure it cleans a populated container
-		 */
-		pool<obj> b(16);
-		b.~pool();
-		TS_ASSERT(is_container_valid(b));
-		TS_ASSERT_EQUALS(b.size(), 0);
-	}
+    /*
+     * Case2: Ensure it cleans a populated container
+     */
+    pool<obj> b(16);
+    b.~pool();
+    TS_ASSERT(is_container_valid(b));
+    TS_ASSERT_EQUALS(b.size(), 0);
+  }
 
-	void test_allocate()
-	{
-		/*
-		 * Case1: Ensure throw keeps container in valid state:
-		 */
-		pool<obj> a;
-		flex::allocation_guard::enable();
-		TS_ASSERT_THROWS(a.allocate(), std::runtime_error)
-		flex::allocation_guard::disable();
-		TS_ASSERT(is_container_valid(a));
+  void test_allocate()
+  {
+    /*
+     * Case1: Ensure throw keeps container in valid state:
+     */
+    pool<obj> a;
+    flex::allocation_guard::enable();
+    TS_ASSERT_THROWS(a.allocate(), std::runtime_error)
+    flex::allocation_guard::disable();
+    TS_ASSERT(is_container_valid(a));
 
-		/*
-		 * Case2: Ensure it works on empty container.
-		 */
-		obj* ptr = (obj*) a.allocate();
-		TS_ASSERT(is_container_valid(a));
-		TS_ASSERT_EQUALS(a.size(), 0);
-		new ((void*) ptr) obj(7);
-		TS_ASSERT_EQUALS(ptr->val, 7);
-		a.deallocate((void*) ptr);
-		TS_ASSERT(is_container_valid(a));
-		TS_ASSERT_EQUALS(a.size(), 1);
+    /*
+     * Case2: Ensure it works on empty container.
+     */
+    obj* ptr = (obj*) a.allocate();
+    TS_ASSERT(is_container_valid(a));
+    TS_ASSERT_EQUALS(a.size(), 0);
+    new ((void*) ptr) obj(7);
+    TS_ASSERT_EQUALS(ptr->val, 7);
+    a.deallocate((void*) ptr);
+    TS_ASSERT(is_container_valid(a));
+    TS_ASSERT_EQUALS(a.size(), 1);
 
-		/*
-		 * Case2: Ensure it works on populated container
-		 */
-		pool<obj> b(16);
-		flex::allocation_guard::enable();
-		ptr = (obj*) b.allocate();
-		TS_ASSERT(is_container_valid(b));
-		TS_ASSERT_EQUALS(b.size(), 15);
-		new ((void*) ptr) obj(7);
-		TS_ASSERT_EQUALS(ptr->val, 7);
-		b.deallocate((void*) ptr);
-		TS_ASSERT(is_container_valid(b));
-		TS_ASSERT_EQUALS(b.size(), 16);
-		flex::allocation_guard::disable();
-	}
+    /*
+     * Case2: Ensure it works on populated container
+     */
+    pool<obj> b(16);
+    flex::allocation_guard::enable();
+    ptr = (obj*) b.allocate();
+    TS_ASSERT(is_container_valid(b));
+    TS_ASSERT_EQUALS(b.size(), 15);
+    new ((void*) ptr) obj(7);
+    TS_ASSERT_EQUALS(ptr->val, 7);
+    b.deallocate((void*) ptr);
+    TS_ASSERT(is_container_valid(b));
+    TS_ASSERT_EQUALS(b.size(), 16);
+    flex::allocation_guard::disable();
+  }
 
-	void test_deallocate()
-	{
+  void test_deallocate()
+  {
 
-	}
+  }
 
-	void test_construct()
-	{
-		/*
-		 * Case1: Ensure it works on empty container.
-		 */
-		pool<obj> a;
-		obj* ptr = a.construct();
-		TS_ASSERT(is_container_valid(a));
-		TS_ASSERT_EQUALS(a.size(), 0);
-		TS_ASSERT_EQUALS(ptr->val, obj::DEFAULT_VAL);
-		a.destruct(ptr);
-		TS_ASSERT(is_container_valid(a));
-		TS_ASSERT_EQUALS(a.size(), 1);
+  void test_construct()
+  {
+    /*
+     * Case1: Ensure it works on empty container.
+     */
+    pool<obj> a;
+    obj* ptr = a.construct();
+    TS_ASSERT(is_container_valid(a));
+    TS_ASSERT_EQUALS(a.size(), 0);
+    TS_ASSERT_EQUALS(ptr->val, obj::DEFAULT_VAL);
+    a.destruct(ptr);
+    TS_ASSERT(is_container_valid(a));
+    TS_ASSERT_EQUALS(a.size(), 1);
 
-		/*
-		 * Case2: Ensure it works on populated container
-		 */
-		pool<obj> b(16);
-		flex::allocation_guard::enable();
-		ptr = b.construct();
-		TS_ASSERT(is_container_valid(b));
-		TS_ASSERT_EQUALS(b.size(), 15);
-		TS_ASSERT_EQUALS(ptr->val, obj::DEFAULT_VAL);
-		b.destruct(ptr);
-		TS_ASSERT(is_container_valid(b));
-		TS_ASSERT_EQUALS(b.size(), 16);
-		flex::allocation_guard::disable();
-	}
+    /*
+     * Case2: Ensure it works on populated container
+     */
+    pool<obj> b(16);
+    flex::allocation_guard::enable();
+    ptr = b.construct();
+    TS_ASSERT(is_container_valid(b));
+    TS_ASSERT_EQUALS(b.size(), 15);
+    TS_ASSERT_EQUALS(ptr->val, obj::DEFAULT_VAL);
+    b.destruct(ptr);
+    TS_ASSERT(is_container_valid(b));
+    TS_ASSERT_EQUALS(b.size(), 16);
+    flex::allocation_guard::disable();
+  }
 
-	void test_construct_val()
-	{
-		/*
-		 * Case1: Ensure it works on empty container.
-		 */
-		pool<obj> a;
-		obj* ptr = a.construct(7);
-		TS_ASSERT(is_container_valid(a));
-		TS_ASSERT_EQUALS(a.size(), 0);
-		TS_ASSERT_EQUALS(ptr->val, 7);
-		a.destruct(ptr);
-		TS_ASSERT(is_container_valid(a));
-		TS_ASSERT_EQUALS(a.size(), 1);
+  void test_construct_val()
+  {
+    /*
+     * Case1: Ensure it works on empty container.
+     */
+    pool<obj> a;
+    obj* ptr = a.construct(7);
+    TS_ASSERT(is_container_valid(a));
+    TS_ASSERT_EQUALS(a.size(), 0);
+    TS_ASSERT_EQUALS(ptr->val, 7);
+    a.destruct(ptr);
+    TS_ASSERT(is_container_valid(a));
+    TS_ASSERT_EQUALS(a.size(), 1);
 
-		/*
-		 * Case2: Ensure it works on populated container
-		 */
-		pool<obj> b(16);
-		flex::allocation_guard::enable();
-		ptr = b.construct(7);
-		TS_ASSERT(is_container_valid(b));
-		TS_ASSERT_EQUALS(b.size(), 15);
-		TS_ASSERT_EQUALS(ptr->val, 7);
-		b.destruct(ptr);
-		TS_ASSERT(is_container_valid(b));
-		TS_ASSERT_EQUALS(b.size(), 16);
-		flex::allocation_guard::disable();
-	}
+    /*
+     * Case2: Ensure it works on populated container
+     */
+    pool<obj> b(16);
+    flex::allocation_guard::enable();
+    ptr = b.construct(7);
+    TS_ASSERT(is_container_valid(b));
+    TS_ASSERT_EQUALS(b.size(), 15);
+    TS_ASSERT_EQUALS(ptr->val, 7);
+    b.destruct(ptr);
+    TS_ASSERT(is_container_valid(b));
+    TS_ASSERT_EQUALS(b.size(), 16);
+    flex::allocation_guard::disable();
+  }
 
-	void test_assignment_operator()
-	{
-		/*
-		 * Case1: Ensure assignment operator is no-op
-		 */
-		pool<obj> a;
-		pool<obj> b(16);
-		a = b;
-		TS_ASSERT(is_container_valid(a));
-		TS_ASSERT_EQUALS(a.size(), 0);
-	}
+  void test_assignment_operator()
+  {
+    /*
+     * Case1: Ensure assignment operator is no-op
+     */
+    pool<obj> a;
+    pool<obj> b(16);
+    a = b;
+    TS_ASSERT(is_container_valid(a));
+    TS_ASSERT_EQUALS(a.size(), 0);
+  }
 
-	void test_reserve()
-	{
-		/*
-		 * Case1: Ensure throw keeps container in valid state:
-		 */
-		pool<obj> a;
-		flex::allocation_guard::enable();
-		TS_ASSERT_THROWS(a.reserve(16), std::runtime_error)
-		flex::allocation_guard::disable();
-		TS_ASSERT(is_container_valid(a));
-		TS_ASSERT_EQUALS(a.size(), 0);
+  void test_reserve()
+  {
+    /*
+     * Case1: Ensure throw keeps container in valid state:
+     */
+    pool<obj> a;
+    flex::allocation_guard::enable();
+    TS_ASSERT_THROWS(a.reserve(16), std::runtime_error)
+    flex::allocation_guard::disable();
+    TS_ASSERT(is_container_valid(a));
+    TS_ASSERT_EQUALS(a.size(), 0);
 
-		/*
-		 * Case2: Ensure it allocates to an empty container
-		 */
-		a.reserve(16);
-		TS_ASSERT(is_container_valid(a));
-		TS_ASSERT_EQUALS(a.size(), 16);
+    /*
+     * Case2: Ensure it allocates to an empty container
+     */
+    a.reserve(16);
+    TS_ASSERT(is_container_valid(a));
+    TS_ASSERT_EQUALS(a.size(), 16);
 
-		/*
-		 * Case3: Ensure it allocates to a populated container
-		 *
-		 */
-		a.reserve(8);
-		TS_ASSERT(is_container_valid(a));
-		TS_ASSERT_EQUALS(a.size(), 24);
-	}
+    /*
+     * Case3: Ensure it allocates to a populated container
+     *
+     */
+    a.reserve(8);
+    TS_ASSERT(is_container_valid(a));
+    TS_ASSERT_EQUALS(a.size(), 24);
+  }
 
 };
